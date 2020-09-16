c5d4ef614e6d1853e2b1fb9d7897d98dadb6d928
Tadeu <tadeufala@gmail.com>
2020-09-16 15:28:10 -0300

    Project refactored and refactored to use MongoDB

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,8 +2,12 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
@@ -12,12 +16,13 @@ import java.math.BigDecimal;
 @Setter
 public class Ingredient {
 
-    private String id;
+    private String id  = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
-    private Recipe recipe;
+//    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -27,12 +32,4 @@ public class Ingredient {
         this.amount = amount;
         this.uom = uom;
     }
-
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-        this.recipe = recipe;
-    }
-
 }
